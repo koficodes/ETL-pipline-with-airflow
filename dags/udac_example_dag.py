6554a5b06e6b9185e3a568fb9cf533b46e8cd515
Paul Karikari <paulkarikari1@gmail.com>
2019-12-17 22:46:30 +0000

    connect tasks

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -58,33 +58,62 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag,
-    redshift_conn_id="redshif",
+    redshift_conn_id="redshift",
     sql_query=SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql_query=SqlQueries.songplay_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql_query=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql_query=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql_query=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    table_list=[ 'artists', 'songplays', 'songs', 'users']
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> create_table 
+
+create_table >> stage_events_to_redshift
+create_table >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -7,19 +7,20 @@ class DataQualityOperator(BaseOperator):
     ui_color = '#89DA59'
 
     @apply_defaults
-    def __init__(self,params = {}, *args, **kwargs):
+    def __init__(self,table_list, *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.params = params
+        self.table_list = table_list
 
         def execute(self, context):
             
-            table = kwargs["params"]["table"]
+            
             redshift_hook = PostgresHook("redshift")
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {table} returned no results")
-            num_records = records[0][0]
-            if num_records < 1:
-                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
-            logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+            for table in self.table_list:
+                records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+                if len(records) < 1 or len(records[0]) < 1:
+                    raise ValueError(f"Data quality check failed. {table} returned no results")
+                num_records = records[0][0]
+                if num_records < 1:
+                    raise ValueError(f"Data quality check failed. {table} contained 0 rows")
+                logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
