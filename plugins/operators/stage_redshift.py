ed8cd1f048595ba7f89ae9bda1304883db53757b
Paul Karikari <paulkarikari1@gmail.com>
2019-12-17 15:10:10 +0000

    add agruments to StageToRedshift constructuor

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,4 +1,5 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -7,15 +8,23 @@ class StageToRedshiftOperator(BaseOperator):
     copy_url = 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_connection_id="",
+                 aws_credential_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 delimiter=",",
+                 ingore_headers=1,
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_connection_id=redshift_connection_id
+        self.aws_credential_id=aws_credential_id
+        self.table=table
+        self.s3_bucket=s3_bucket
+        self.s3_key=s3_key
+        self.delimiter=delimiter
+        self.ignore_headers=ignore_headers
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
